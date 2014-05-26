b6b97113d94f70098dae390dc685ccfedfd68805
Henry Cogo <henryjcogo@Henrys-Mac-mini.local>
2014-05-25 22:21:09 -1000

    Initial Commit

---
+++ TimeAfterTime/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TimeAfterTime
+//
+//  Created by Henry Cogo on 5/25/14.
+//  Copyright (c) 2014 Henry Cogo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
