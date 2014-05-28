41bec145c91a61657ca3ca272a9a08a5d7d1bc14
Henry J Cogo <hcogo.github@gmail.com>
2014-05-28 10:27:19 -1000

    Added Comments
    Added date of modification

--- TimeAfterTime/main.m
+++ TimeAfterTime/main.m
@@ -1,22 +1,25 @@
-//
-//  main.m
-//  TimeAfterTime
-//
-//  Created by Henry Cogo on 5/25/14.
-//  Copyright (c) 2014 Henry Cogo. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
-
-int main(int argc, const char * argv[])
+// Modified by Henry Cogo
+//
+int main (int argc, const char *argv[])
 {
-
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSDate *now = [[NSDate alloc] init];
+        NSLog(@"The new date lives at %p", now);
+        NSLog(@"The date is %@", now);
+        NSLog(@"Hello, World!\n");
         
+        double seconds = [now timeIntervalSince1970];
+        NSLog(@"It has veen %.2f seconds since the start of 1970.", seconds);
+        
+        NSDate *later = [now dateByAddingTimeInterval:100000];
+        NSLog(@"In 100,000 seconds it will be %@.", later);
+        
+        NSCalendar *cal = [NSCalendar currentCalendar];
+        NSUInteger day = [cal ordinalityOfUnit:NSDayCalendarUnit
+                                        inUnit:NSMonthCalendarUnit forDate:now];
+        NSLog(@"This is day %lu of the month.", day);
     }
     return 0;
 }
-
